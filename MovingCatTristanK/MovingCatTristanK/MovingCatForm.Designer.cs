8dd95e433b92573c1488631117b993ed10e83ed3
Tristan Kalabric <32547188+TristanKalabric@users.noreply.github.com>
2018-09-25 11:03:56 -0400

    Moved into unit folder

---
+++ MovingCatTristanK/MovingCatTristanK/MovingCatForm.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace MovingCatTristanK
+{
+    partial class frmMovingCat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mnuStrip = new System.Windows.Forms.MenuStrip();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniCat = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniCat1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniCat2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.picMovingCat = new System.Windows.Forms.PictureBox();
+            this.mnuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picMovingCat)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mnuStrip
+            // 
+            this.mnuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile,
+            this.mniCat});
+            this.mnuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mnuStrip.Name = "mnuStrip";
+            this.mnuStrip.Size = new System.Drawing.Size(284, 24);
+            this.mnuStrip.TabIndex = 0;
+            this.mnuStrip.Text = "Menu Strip";
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
+            this.mniFile.Text = "File";
+            // 
+            // mniCat
+            // 
+            this.mniCat.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniCat1,
+            this.mniCat2});
+            this.mniCat.Name = "mniCat";
+            this.mniCat.Size = new System.Drawing.Size(37, 20);
+            this.mniCat.Text = "Cat";
+            // 
+            // mniCat1
+            // 
+            this.mniCat1.Name = "mniCat1";
+            this.mniCat1.Size = new System.Drawing.Size(152, 22);
+            this.mniCat1.Text = "Cat1";
+            this.mniCat1.Click += new System.EventHandler(this.mniCat1_Click);
+            // 
+            // mniCat2
+            // 
+            this.mniCat2.Name = "mniCat2";
+            this.mniCat2.Size = new System.Drawing.Size(152, 22);
+            this.mniCat2.Text = "Cat2";
+            this.mniCat2.Click += new System.EventHandler(this.mniCat2_Click);
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(152, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // picMovingCat
+            // 
+            this.picMovingCat.Image = global::MovingCatTristanK.Properties.Resources.cat2;
+            this.picMovingCat.Location = new System.Drawing.Point(78, 79);
+            this.picMovingCat.Name = "picMovingCat";
+            this.picMovingCat.Size = new System.Drawing.Size(130, 132);
+            this.picMovingCat.TabIndex = 1;
+            this.picMovingCat.TabStop = false;
+            // 
+            // frmMovingCat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.picMovingCat);
+            this.Controls.Add(this.mnuStrip);
+            this.MainMenuStrip = this.mnuStrip;
+            this.Name = "frmMovingCat";
+            this.Text = "Moving Cat";
+            this.mnuStrip.ResumeLayout(false);
+            this.mnuStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picMovingCat)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnuStrip;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+        private System.Windows.Forms.ToolStripMenuItem mniCat;
+        private System.Windows.Forms.ToolStripMenuItem mniCat1;
+        private System.Windows.Forms.ToolStripMenuItem mniCat2;
+        private System.Windows.Forms.PictureBox picMovingCat;
+    }
+}
+
---
+++ MovingCatTristanK/MovingCatTristanK/MovingCatForm.cs
@@ -0,0 +1,45 @@
+﻿/*Created by: Tristan Kalabric
+ * Created on: September 17, 2018
+ * Created for: ICS3U programming
+ * Daily Assignment: - Day #8 - Moving Cat
+ * This program displays 2 pictures of a car that changes depending on the button you click
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovingCatTristanK
+{
+    public partial class frmMovingCat : Form
+    {
+        public frmMovingCat()
+        {
+            InitializeComponent();
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            //When clicked, program closes
+            this.Close();
+        }
+
+        private void mniCat1_Click(object sender, EventArgs e)
+        {
+            //When clicked, the picture of the cat changes
+            this.picMovingCat.Image = Properties.Resources.cat1;
+        }
+
+        private void mniCat2_Click(object sender, EventArgs e)
+        {
+            //When clicked, the picture of the cat changes
+            this.picMovingCat.Image = Properties.Resources.cat2;
+        }
+    }
+}
